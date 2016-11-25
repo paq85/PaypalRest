c8d7d8cda5744057726b1424a727bae12608a9ea
damian.sromek <damian.sromek@gmail.com>
2016-11-25 11:23:02 +0100

    SyncAction updates Payment model amount

--- Action/SyncAction.php
+++ Action/SyncAction.php
@@ -8,6 +8,7 @@ use Payum\Core\Bridge\Spl\ArrayObject;
 use Payum\Core\Exception\RequestNotSupportedException;
 use Payum\Core\GatewayAwareInterface;
 use Payum\Core\GatewayAwareTrait;
+use Payum\Core\Model\Payment;
 use Payum\Core\Request\Sync;
 
 class SyncAction implements ActionInterface, GatewayAwareInterface
@@ -25,12 +26,34 @@ class SyncAction implements ActionInterface, GatewayAwareInterface
         /** @var ArrayObject $model */
         $model = $request->getModel();
 
+        $amount = null;
+        $currency = null;
         $payPalPayment = PaypalPayment::get($model['id']);
+        if ('approved' === $payPalPayment->getState()) {
+            $transactions = $payPalPayment->getTransactions();
+            foreach ($transactions as $transaction) {
+                $amount = $transaction->getAmount()->getTotal();
+                $currency = $transaction->getAmount()->getCurrency();
+            }
+
+            /* @var Payment $paymentModel */
+            $paymentModel = $request->getFirstModel();
+            $amountInCents = (int) str_replace('.', '', $amount);
+            if ($paymentModel->getTotalAmount() !== $amountInCents) {
+                $paymentModel->setTotalAmount($amountInCents);
+            }
+            if ($paymentModel->getCurrencyCode() !== $currency) {
+                $paymentModel->setCurrencyCode($currency);
+            }
+        }
+
         $data = [
-            'id' => $payPalPayment->getId(),
-            'create_time'   => $payPalPayment->getCreateTime(),
-            'update_time'   => $payPalPayment->getUpdateTime(),
-            'state'         => $payPalPayment->getState() // "approved" means it was OK
+            'id'                => $payPalPayment->getId(),
+            'create_time'       => $payPalPayment->getCreateTime(),
+            'update_time'       => $payPalPayment->getUpdateTime(),
+            'amount'            => $amount,
+            'currency'          => $currency,
+            'state'             => $payPalPayment->getState() // "approved" means it was OK
         ];
         $model->replace($data);
     }
